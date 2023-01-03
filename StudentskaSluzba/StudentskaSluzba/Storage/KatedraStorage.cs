e95b2db5027e017ab8874bd0789efcab4c6603f1
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-01-03 17:12:35 +0100

    Implemented storage

--- StudentskaSluzba/StudentskaSluzba/Storage/KatedraStorage.cs
+++ StudentskaSluzba/StudentskaSluzba/Storage/KatedraStorage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StudentskaSluzba.Model;
+using StudentskaSluzba.Serializer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,23 @@ namespace StudentskaSluzba.Storage
 {
     internal class KatedraStorage
     {
+        private const string StoragePath = "../../Data/katedre.csv";
+
+        private Serializer<Katedra> _serializer;
+
+        public KatedraStorage()
+        {
+            _serializer = new Serializer<Katedra>();
+        }
+
+        public List<Katedra> Load()
+        {
+            return _serializer.FromCSV(StoragePath);
+        }
+
+        public void Save(List<Katedra> katedre)
+        {
+            _serializer.ToCSV(StoragePath, katedre);
+        }
     }
 }
--- StudentskaSluzba/StudentskaSluzba/Storage/PredmetStorage.cs
+++ StudentskaSluzba/StudentskaSluzba/Storage/PredmetStorage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StudentskaSluzba.Model;
+using StudentskaSluzba.Serializer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,24 @@ namespace StudentskaSluzba.Storage
 {
     internal class PredmetStorage
     {
+        private const string StoragePath = "../../Data/predmeti.csv";
+
+        private Serializer<Predmet> _serializer;
+
+
+        public PredmetStorage()
+        {
+            _serializer = new Serializer<Predmet>();
+        }
+
+        public List<Predmet> Load()
+        {
+            return _serializer.FromCSV(StoragePath);
+        }
+
+        public void Save(List<Predmet> predmeti)
+        {
+            _serializer.ToCSV(StoragePath, predmeti);
+        }
     }
 }
