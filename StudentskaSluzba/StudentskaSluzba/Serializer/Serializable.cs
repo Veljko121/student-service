f02d66cb6ce329a9287f22b28c8027af83555c5c
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-01-03 12:30:56 +0100

    Update Serializable.cs

--- StudentskaSluzba/StudentskaSluzba/Serializer/Serializable.cs
+++ StudentskaSluzba/StudentskaSluzba/Serializer/Serializable.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StudentskaSluzba.Serializer
+﻿namespace StudentskaSluzba.Serializer
 {
     internal interface ISerializable
     {
